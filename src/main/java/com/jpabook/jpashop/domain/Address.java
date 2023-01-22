f36ad0f18cce18ac8a21c622fb67465ca0550d5d
EJ <hanej93@naver.com>
2023-01-22 16:43:12 +0900

    엔티티 클래스 개발2

--- src/main/java/com/jpabook/jpashop/domain/Address.java
+++ src/main/java/com/jpabook/jpashop/domain/Address.java
@@ -1,13 +1,17 @@
 package com.jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
-public
-class Address {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Address {
 
     private String city;
     private String street;
