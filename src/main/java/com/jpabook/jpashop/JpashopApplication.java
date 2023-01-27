45367d494dfde606781b3159ce0653ec812004cf
EJ <hanej93@naver.com>
2023-01-27 21:35:26 +0900

    주문 조회 V1: 엔티티 직접 노출

--- src/main/java/com/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/com/jpabook/jpashop/JpashopApplication.java
@@ -13,7 +13,7 @@ public class JpashopApplication {
 	}
 
 	@Bean
-	Hibernate5Module hibernate5Module() {
+	Hibernate5Module hibernate5Module() { // 프록시 데이터는 조회하지 않음
 		Hibernate5Module hibernate5Module = new Hibernate5Module();
 		// 조회하기 전에 강제로 LAZY LOADING 실행
 //		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
